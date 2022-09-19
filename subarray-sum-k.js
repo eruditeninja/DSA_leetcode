8be582ad47f7909a0826c44adc750b367b7c8b92
Neeraj <neerajgupta.cn@propertyguru.tech>
2022-09-19 18:38:54 +0530

    [Array][Easy-level]-striver -Completed

--- subarray-sum-k.js
+++ subarray-sum-k.js
@@ -1,26 +1,20 @@
+var subarraySum = function(nums, k) {
 
-let arr =[10, 2, -2, -20, 10],end=-1,totalCount=0;
-let k=-10,maxCount=0;
-let sum=0;
-
-for (let start = 0; start < arr.length; start++) {
+    let sum = 0
+    let count = 0
+      const map = new Map()
+      for (let i = 0; i < nums.length; i++){
+        if (!map.has(sum)){                      
+          map.set(sum, 1)                        
+        } else {
+          map.set(sum, map.get(sum) + 1)         
+        }
     
-    while(( arr[end+1] +sum<=k) && end+1<arr.length ){
-        sum+=arr[++end];
-    }
-
-    if(sum==k) {
-        totalCount++;
-        maxCount = end-start+1 > maxCount? end-start+1:maxCount; 
-    }
-    sum-=arr[start];
-}
-
-console.log('count',maxCount,totalCount,'totalCount');
-
-// Input : arr[] = {10, 2, -2, -20, 10}, k = -10
-// Output : 3
-// Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
-
-// Input : arr[] = {9, 4, 20, 3, 10, 5}, k = 33
-
+        sum += nums[i]                           
+    
+        if (map.has(sum-k)){                     
+          count += map.get(sum-k)               
+        }
+      }
+      return count   
+    };
